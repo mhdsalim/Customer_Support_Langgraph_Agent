d15e7cfde00d15b777d23aabb3088aeea4127595
Muhammed Salim <salimmhdkeloth@gmail.com>
2025-08-29 23:02:32 +0530

    Update plot_workflow.py

--- agent/plot_workflow.py
+++ agent/plot_workflow.py
@@ -1,5 +1,10 @@
-from IPython.display import Image
 from .workflow import app
 
-# Render inline
-Image(app.get_graph().draw_mermaid_png())
+# Generate the PNG
+png_data = app.get_graph().draw_mermaid_png()
+
+# Save to file
+with open("workflow.png", "wb") as f:
+    f.write(png_data)
+
+print("Graph saved as workflow.png")
